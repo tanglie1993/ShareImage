9fcbe5cd636ba11d5752c3bf925f0036dae9aed9
tanglie1993 <tanglie23@163.com>
2017-07-30 00:04:36 +0800

    新建android项目

---
+++ frontend/MyApplication/app/src/main/java/com/tanglie1993/shareimage/MainActivity.java
@@ -0,0 +1,19 @@
+package com.tanglie1993.shareimage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
