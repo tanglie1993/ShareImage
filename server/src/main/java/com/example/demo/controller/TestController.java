45ae6fd2a63069824f90db2ddbad6be5054146d1
tanglie1993 <tanglie23@163.com>
2017-07-29 12:41:14 +0800

    实现图片下载

--- server/src/main/java/com/example/demo/controller/TestController.java
+++ server/src/main/java/com/example/demo/controller/TestController.java
@@ -4,14 +4,11 @@ package com.example.demo.controller;
 import com.example.demo.dao.ImagesDao;
 import com.example.demo.entity.ImagesEntity;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 
 @RestController
 public class TestController {
@@ -30,15 +27,21 @@ public class TestController {
     }
 
     //文件上传相关代码
-    @RequestMapping(value = "upload")
+    @RequestMapping(value = "image", method = RequestMethod.POST)
     @ResponseBody
-    public String upload(@RequestParam("file") MultipartFile file,
-                         @RequestParam("user_id") Integer userId) {
+    public String upload(@RequestParam(value = "file", required = true) MultipartFile file,
+                         @RequestParam(value = "user_id", required = true) Integer userId,
+                         @RequestParam(value = "timestamp", required = false) Integer timestamp) {
         if (file.isEmpty()) {
             return "文件为空";
         }
         // 获取文件名
-        String fileName = "" + System.currentTimeMillis() + ".png";
+        String fileName;
+        if(timestamp != null){
+            fileName = "" + timestamp + ".png";
+        }else{
+            fileName = "" + System.currentTimeMillis() + ".png";
+        }
         String filePath = "//root//images//" + userId + "//";
         if(!new File(filePath).exists()){
             new File(filePath).mkdirs();
@@ -63,36 +66,37 @@ public class TestController {
         return "上传失败";
     }
 //
-//    @RequestMapping(value = "/testDownload", method = RequestMethod.GET)
-//    public void testDownload(@RequestParam ("user_id") Integer userId,
-//                             HttpServletResponse res) {
-//        String fileName = "//root//images//" + userId + "//" + imagesDao.findByUserId(userId).get(0).getTimestamp() + ".png";
-//        res.setHeader("content-type", "application/octet-stream");
-//        res.setContentType("application/octet-stream");
-//        res.setHeader("Content-Disposition", "attachment;filename=" + fileName);
-//        byte[] buff = new byte[1024];
-//        BufferedInputStream bis = null;
-//        OutputStream os = null;
-//        try {
-//            os = res.getOutputStream();
-//            bis = new BufferedInputStream(new FileInputStream(new File(fileName)));
-//            int i = bis.read(buff);
-//            while (i != -1) {
-//                os.write(buff, 0, buff.length);
-//                os.flush();
-//                i = bis.read(buff);
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } finally {
-//            if (bis != null) {
-//                try {
-//                    bis.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }
-//        System.out.println("success");
-//    }
+    @RequestMapping(value = "/image", method = RequestMethod.GET)
+    public void testDownload(@RequestParam ("user_id") Integer userId,
+                             @RequestParam ("timestamp") Long timestamp,
+                             HttpServletResponse res) {
+        String fileName = "//root//images//" + userId + "//" + timestamp + ".png";
+        res.setHeader("content-type", "application/octet-stream");
+        res.setContentType("application/octet-stream");
+        res.setHeader("Content-Disposition", "attachment;filename=" + fileName);
+        byte[] buff = new byte[1024];
+        BufferedInputStream bis = null;
+        OutputStream os = null;
+        try {
+            os = res.getOutputStream();
+            bis = new BufferedInputStream(new FileInputStream(new File(fileName)));
+            int i = bis.read(buff);
+            while (i != -1) {
+                os.write(buff, 0, buff.length);
+                os.flush();
+                i = bis.read(buff);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bis != null) {
+                try {
+                    bis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        System.out.println("success");
+    }
 }
