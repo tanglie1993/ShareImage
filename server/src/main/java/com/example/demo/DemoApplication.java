1c71018861b19f3800dc396c462049d32cdd2935
tanglie1993 <tanglie23@163.com>
2017-07-29 18:47:55 +0800

    允许跨域请求

--- server/src/main/java/com/example/demo/DemoApplication.java
+++ server/src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@ServletComponentScan
 public class DemoApplication {
 
 	@RequestMapping("/h")
---
+++ server/src/main/java/com/example/demo/filter/IndexFilter.java
@@ -0,0 +1,38 @@
+package com.example.demo.filter;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/*", filterName = "indexFilter")
+public class IndexFilter implements Filter {
+    Log log = LogFactory.getLog(IndexFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("init IndexFilter");
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        HttpServletRequest reqs = (HttpServletRequest) req;
+
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
