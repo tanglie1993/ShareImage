d4a5462d8d79fa7f99a1f69860c07c0cf7400966
tanglie1993 <tanglie23@163.com>
2017-08-05 23:28:57 +0800

    实现网络数据下载

--- RNDemo/index.android.js
+++ RNDemo/index.android.js
@@ -6,10 +6,10 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
     TextInput,
     ScrollView,
     Image
@@ -18,26 +18,65 @@ import {
 export default class RNDemo extends Component {
     constructor(props) {
         super(props);
-        this.state = {text: ''};
+        console.log("00000000")
+        this.state = {
+            content: "",
+        };
     }
 
-    render() {
+    componentDidMount()
+    {
+        fetch('http://116.62.134.157:80/imageList?user_id=42')
+            .then((response) => response.json())
+            .then((responseJson) => {
+                console.log("aaaaaa")
+                this.setState({
+                    content:JSON.stringify(responseJson)
+                });
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    render()
+    {
+        if (!this.state.content) {
+            return this.renderLoadingView();
+        }
+        return this.renderContent(this.state.content);
+    }
+
+    renderLoadingView()
+    {
+        return (
+            <View style={styles.container}>
+              <Text>
+                正在加载数据......
+              </Text>
+            </View>
+        );
+    }
+
+
+    renderContent(text) {
+        console.log("ddddddddddd")
         let uri = 'http://116.62.134.157:80/image/42/1501339989343.png';
         let height = 100;
         let width = 100;
         return(
             <ScrollView>
-              <Text style={{fontSize:96}}>Scroll me plz</Text>
+              <Text style={{fontSize:16}}>{text}</Text>
               <Image source={{uri: uri}}
-                  style={{width: width, height: height}} />
-                <Image source={{uri: uri}}
-                style={{width: width, height: height}} />
-                <Image source={{uri: uri}}
-                       style={{width: width, height: height}} />
-                <Image source={{uri: uri}}
-                       style={{width: width, height: height}} />
-                <Image source={{uri: uri}}
-                       style={{width: width, height: height}} />
+                     style={{width: width, height: height}} />
+              <Image source={{uri: uri}}
+                     style={{width: width, height: height}} />
+              <Image source={{uri: uri}}
+                     style={{width: width, height: height}} />
+              <Image source={{uri: uri}}
+                     style={{width: width, height: height}} />
+              <Image source={{uri: uri}}
+                     style={{width: width, height: height}} />
               <Image source={{uri: uri}}
                      style={{width: width, height: height}} />
               <Image source={{uri: uri}}
@@ -65,22 +104,22 @@ export default class RNDemo extends Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
 
 AppRegistry.registerComponent('RNDemo', () => RNDemo);
