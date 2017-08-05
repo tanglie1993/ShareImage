a64bfcd0c67f04978f1273aaf26e39da4dd1485b
tanglie1993 <tanglie23@163.com>
2017-08-05 22:16:29 +0800

    修改图片数据

--- server/src/main/java/com/example/demo/constants/FileConstants.java
+++ server/src/main/java/com/example/demo/constants/FileConstants.java
@@ -2,8 +2,8 @@ package com.example.demo.constants;
 
 public class FileConstants {
 
-        public static final String ROOT_IMAGES = "d://images//";
-//    public static final String ROOT_IMAGES = "//root//images//";
+//        public static final String ROOT_IMAGES = "d://images//";
+    public static final String ROOT_IMAGES = "//root//images//";
 
     public static String getThumbnailAddress(int userId, long timeStamp){
         return FileConstants.ROOT_IMAGES + userId + "//thumb_" + timeStamp;
