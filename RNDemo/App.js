259e4a7358b7805efdb5544f3d20fb0bae998c9d
tanglie1993 <tanglie23@163.com>
2017-08-13 20:49:57 +0800

    重构

---
+++ RNDemo/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+    AppRegistry,
+    Text,
+    View,
+    Button
+} from 'react-native';
+import { StackNavigator } from 'react-navigation';
+
+class HomeScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Welcome',
+    };
+    render() {
+        const { navigate } = this.props.navigation;
+        return (
+            <View>
+                <Text>Hello, Chat App!</Text>
+                <Button
+                    onPress={() => navigate('Chat')}
+                    title="Chat with Lucy"
+                />
+            </View>
+        );
+    }
+}
+
+class ChatScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Chat with Lucy',
+    };
+    render() {
+        return (
+            <View>
+                <Text>Chat with Lucy</Text>
+            </View>
+        );
+    }
+}
+
+const RNDemo = StackNavigator({
+    Home: { screen: HomeScreen },
+    Chat: { screen: ChatScreen },
+});
+AppRegistry.registerComponent('RNDemo', () => RNDemo);
