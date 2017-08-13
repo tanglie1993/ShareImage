f289e517e7b6dbb6336a506b02779d7358ae8e3f
tanglie1993 <tanglie23@163.com>
2017-08-13 21:14:55 +0800

    恢复原页面显示

--- RNDemo/HomeScreen.js
+++ RNDemo/HomeScreen.js
@@ -1,21 +1,108 @@
-import React from 'react';
-import {Button, Text, View} from 'react-native';
-import {StackNavigator} from 'react-navigation';
 
-export class HomeScreen extends React.Component {
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    ScrollView,
+    Image,
+    FlatList,
+    TouchableOpacity
+} from 'react-native';
+
+export class HomeScreen extends Component {
+
     static navigationOptions = {
         title: 'Welcome',
     };
-    render() {
-        const { navigate } = this.props.navigation;
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            listData : null
+        };
+    }
+
+    componentDidMount()
+    {
+        fetch('http://116.62.134.157:80/imageList?user_id=42')
+            .then((response) => response.json())
+            .then((responseJson) => {
+                this.setState({
+                    listData:responseJson.list
+                });
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    render()
+    {
+        if (!this.state.listData) {
+            return this.renderLoadingView();
+        }
+        return this.renderContent(this.state.listData);
+    }
+
+    renderLoadingView()
+    {
         return (
-            <View>
-                <Text>Hello, Chat App!</Text>
-                <Button
-                    onPress={() => navigate('Chat')}
-                    title="Chat with Lucy"
-                />
+            <View style={styles.container}>
+                <Text>
+                    正在加载数据......
+                </Text>
             </View>
         );
     }
+
+    renderContent(listData) {
+        const { navigate } = this.props.navigation;
+        let uri = 'http://116.62.134.157:80/image/42/1501339989343.png';
+        let height = 100;
+        let width = 100;
+        return(
+            <ScrollView>
+
+                <View style={styles.container}>
+
+                    <View style={styles.container}>
+                        <FlatList
+                            data={listData}
+                            renderItem={({item}) =>
+                                <TouchableOpacity onPress={() => navigate('Chat')}>
+                                    <Image
+                                        style={{width: width, height: height}}
+                                        source={{uri: 'http://116.62.134.157:80/image/42/' + item.imageUrl + '.png'}}
+                                    />
+                                </TouchableOpacity>
+                            }
+                        />
+                    </View>
+                </View>
+                <Text style={{fontSize:80}}>React Native</Text>
+            </ScrollView>
+        );
+    }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
