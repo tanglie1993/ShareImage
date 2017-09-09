f0db80907d39ce19cf37e3e327c348dce66f0f91
tanglie <tanglie23@163.com>
2017-09-09 23:31:07 +0800

    修改ip地址

--- RNDemo/Constants.js
+++ RNDemo/Constants.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export class Constants extends React.Component {
 
-    static BASE_URL = "121.196.220.217:9090/";
+    static BASE_URL = "http://121.196.220.217:9090/";
     static USER_ID = 42;
 
 }
