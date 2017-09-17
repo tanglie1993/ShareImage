72f70526c6dd4847c635e05c01f7b8dd31abef6a
tanglie <tanglie23@163.com>
2017-09-17 23:34:01 +0800

    添加inputtext

--- RNDemo/UploadScreen.js
+++ RNDemo/UploadScreen.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import {Image} from 'react-native';
-import {StackNavigator} from 'react-navigation';
-import {Constants} from "./Constants";
+import {Image, TextInput, View} from 'react-native';
 
 export class UploadScreen extends React.Component {
 
@@ -12,12 +10,32 @@ export class UploadScreen extends React.Component {
         title: 'Upload',
     };
 
+    constructor(props) {
+        super(props);
+        this.state = { text: 'Useless Placeholder' };
+    }
+
     render() {
         return (
-            <Image
-                style={{width: this.WIDTH, height: this.HEIGHT}}
-                source={{uri: this.props.navigation.state.params.param}}
-            />
+            <View>
+                <Image
+                    style={{width: this.WIDTH, height: this.HEIGHT}}
+                    source={{uri: this.props.navigation.state.params.param}}
+                />
+
+                <TextInput
+                    style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                    onChangeText={(text) => this.setState({text})}
+                    value={this.state.text}
+                />
+
+                <TextInput
+                    style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                    onChangeText={(text) => this.setState({text})}
+                    value={this.state.text}
+                />
+            </View>
+
         );
     }
 }
