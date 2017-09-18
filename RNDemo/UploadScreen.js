e038962a97c4c42f8689784f4b768b8a88496f77
tanglie <tanglie23@163.com>
2017-09-18 22:38:55 +0800

    实现图片上传

--- RNDemo/UploadScreen.js
+++ RNDemo/UploadScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {Image, TextInput, View} from 'react-native';
+import {Constants} from "./Constants";
+import {Image, TextInput, View, Button} from 'react-native';
 
 export class UploadScreen extends React.Component {
 
-    HEIGHT = 500;
-    WIDTH = 500;
+    HEIGHT = 300;
+    WIDTH = 300;
 
     static navigationOptions = {
         title: 'Upload',
@@ -12,7 +13,32 @@ export class UploadScreen extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { text: 'Useless Placeholder' };
+        this.state = {
+            title: 'Title',
+            content: 'Content'
+        };
+    }
+
+    uploadPic(){
+        let formData = new FormData();
+        let file = {uri: this.props.navigation.state.params.param, type: 'multipart/form-data', name: '354365789'};
+
+        formData.append("file",file);
+
+        fetch(Constants.BASE_URL + "image?user_id=" + Constants.USER_ID,{
+            method:'POST',
+            headers:{
+                'Content-Type':'multipart/form-data',
+            },
+            body:formData,
+        })
+            .then((response) => response.text() )
+            .then((responseData)=>{
+
+                console.log('responseData',responseData);
+            })
+            .catch((error)=>{console.error('error',error)});
+
     }
 
     render() {
@@ -25,14 +51,21 @@ export class UploadScreen extends React.Component {
 
                 <TextInput
                     style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-                    onChangeText={(text) => this.setState({text})}
-                    value={this.state.text}
+                    onChangeText={(text) => this.state.title = text}
+                    value={this.state.title}
                 />
 
                 <TextInput
                     style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-                    onChangeText={(text) => this.setState({text})}
-                    value={this.state.text}
+                    onChangeText={(text) => this.state.content = text}
+                    value={this.state.content}
+                />
+
+                <Button
+                    onPress={() => this.uploadPic()}
+                    title="Learn More"
+                    color="#841584"
+                    accessibilityLabel="Learn more about this purple button"
                 />
             </View>
 
