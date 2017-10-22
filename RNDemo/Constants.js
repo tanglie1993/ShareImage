17176a2ee23a6534e90b8a614fdea622296b07ec
tanglie <tanglie23@163.com>
2017-10-22 23:38:07 +0800

    补上传

--- RNDemo/Constants.js
+++ RNDemo/Constants.js
@@ -3,6 +3,7 @@ import React from 'react';
 export class Constants extends React.Component {
 
     static BASE_URL = "http://121.196.220.217:9090/";
+    // static BASE_URL = "http://localhost:8080/";
     static USER_ID = 42;
 
 }
---
+++ server/src/main/java/com/example/demo/entity/UploadComment.java
@@ -0,0 +1,19 @@
+package com.example.demo.entity;
+
+public class UploadComment {
+    private Integer postId;
+    private Integer userId;
+    private String comment;
+
+    public Integer getPostId() {
+        return postId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+}
